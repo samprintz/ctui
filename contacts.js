2e2ca77289eeaac5aed0d7512bdfe756c3ca5d58
Samuel Printz <sam.printz@web.de>
2018-07-22 17:07:48 +0200

    Add and remove contacts.

--- contacts.js
+++ contacts.js
@@ -14,43 +14,80 @@ program
   .option('-r, --remove [name]', 'Remove contact')
   .parse(process.argv);
 
+
+
 if (program.show) {
-  console.log('Kontakte:');
 
-
+  // read contacts into store
+  const parser = N3.Parser();
+  const rdfStream = fs.createReadStream('out.n3');
+  parser.parse(rdfStream, (error, quad, prefixes) => {
+    if (quad) {
+      store.addQuad(quad);
+    } else { // finished
+      console.log('Contacts:');
+      printContacts();
+    }
+  });
 
 }
 
 
 
 if (program.add) {
-  console.log('Füge hinzu: %s', program.add);
 
-  const myQuad = quad(
+  // create new contact
+  var newContact = quad(
     blankNode(),
     namedNode('http://xmlns.com/foaf/0.1/givenName'),
     literal(program.add),
     defaultGraph(),
   );
 
-  store.addQuad(myQuad);
-
-  const myQuad2 = quad(
-    blankNode(),
-    namedNode('http://xmlns.com/foaf/0.1/givenName'),
-    literal("Peter"),
-    defaultGraph(),
-  );
-
-  store.addQuad(myQuad2);
-
-  saveContacts();
+  // read contacts into store and add new contact
+  const parser = N3.Parser();
+  const rdfStream = fs.createReadStream('out.n3');
+  parser.parse(rdfStream, (error, quad, prefixes) => {
+    if (quad) {
+      store.addQuad(quad);
+    } else { // finished
+      const contactAlreadyExists = store.getQuads(null, null, literal(program.add))[0];
+      if (!contactAlreadyExists) {
+        store.addQuad(newContact);
+        console.log('„%s“ added.', program.add);
+        saveContacts();
+      } else {
+        console.log('„%s“ already exists.', program.add);
+      }
+    }
+  });
 
 }
+
+
 
 if (program.remove) {
-  console.log('Lösche: %s', program.remove);
+
+  const parser = N3.Parser();
+  const rdfStream = fs.createReadStream('out.n3');
+  parser.parse(rdfStream, (error, quad, prefixes) => {
+    if (quad) {
+      store.addQuad(quad);
+    } else { // finished
+      const removedContact = store.getQuads(null, null, literal(program.remove))[0];
+      if (removedContact) {
+        store.removeQuad(removedContact);
+        console.log('Delete:');
+        printContact(removedContact);
+        saveContacts();
+      } else {
+        console.log('„%s“ doesn\'t exist.', program.remove);
+      }
+    }
+  });
 }
+
+
 
 
 function saveContacts() {
@@ -58,5 +95,13 @@ function saveContacts() {
   const writer = N3.Writer(writeStream, { end: false, prefixes: { c: 'http://example.org/cartoons#' } });
   store.forEach(quad => writer.addQuad(quad));
   writer.end();
-  console.log("Contacts saved");
+}
+
+function printContact(contact) {
+  var name = contact.object.value;
+  console.log(name);
+}
+
+function printContacts() {
+  store.forEach(quad => printContact(quad));
 }
