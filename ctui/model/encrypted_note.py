d6d7547d9308a422705110289682899e7bb5138e
Samuel Printz <sam.printz@web.de>
2024-11-04 21:54:28 +0100

    Fix encrypted note (old) commands

--- ctui/model/encrypted_note.py
+++ ctui/model/encrypted_note.py
@@ -1,20 +1,11 @@
-from datetime import datetime
-
 from ctui.model.note import Note
 
 
 class EncryptedNote(Note):
 
-    def __init__(self, note_id, content=None, encrypted=True):
-        # TODO refactor like Note
-        if isinstance(note_id, str):
-            try:
-                note_id = datetime.strptime(note_id, '%Y%m%d')
-            except ValueError:
-                raise ValueError(
-                    f'Invalid note name: "{note_id}". Note names match the pattern YYYYMMMDD')
+    def __init__(self, note_id, content=None):
+        super().__init__(note_id, content)
+        super().validate_name(note_id)
+
         self.note_id = note_id
         self.content = content
-
-    def __eq__(self, other):
-        return self.note_id == other.note_id
