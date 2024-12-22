eb0b474185fda2a318decfd415c0b3a8dace283f
Samuel Printz <sam.printz@web.de>
2024-12-22 22:10:35 +0100

    ref: reference tab buttons with ids

--- ctui/component/contact_details_frame.py
+++ ctui/component/contact_details_frame.py
@@ -5,11 +5,13 @@ from ctui.component.contact_details import AttributeDetails, GiftDetails, \
 
 
 class CDetailTabNavigation(urwid.Columns):
-    def __init__(self, tab_names, callback):
-        self.buttons = [
-            CTabButton(name, on_press=callback, user_data=name)
-            for name in tab_names
-        ]
+    def __init__(self, tabs, callback):
+        self.buttons = []
+
+        for tab in tabs:
+            button = CTabButton(tab.tab_name, on_press=callback,
+                                user_data=tab.tab_id)
+            self.buttons.append(button)
 
         self.update_selected_tab_button(0)
 
@@ -50,12 +52,6 @@ class CDetailsFrame(urwid.Frame):
 
         self.current_tab = 0
 
-    def on_tab_click(self, button, tab_name):
-        # TODO merge with set_tab?
-        tab_pos = self.get_tab_pos_by_name(tab_name)
-        self.body.original_widget = self.tabs[tab_pos]
-        self.header.update_selected_tab_button(self.current_tab)
-
     def set_contact(self, contact_id: str) -> None:
         self.tabs = [
             NoteDetails(contact_id, self.core),
@@ -63,8 +59,7 @@ class CDetailsFrame(urwid.Frame):
             AttributeDetails(contact_id, self.core),
         ]
 
-        self.header = CDetailTabNavigation(self.get_tab_names(),
-                                           self.on_tab_click)
+        self.header = CDetailTabNavigation(self.tabs, self.on_tab_click)
         self.body = CDetailTabBody(self.tabs[0])
 
         # keep the latest selected tab selected after detail updates
@@ -73,9 +68,6 @@ class CDetailsFrame(urwid.Frame):
     def get_tab_body(self):
         # TODO rename to get_current_tab()? and get_current_tab() to get_current_tab_id()?
         return self.body.original_widget
-
-    def get_tab_names(self):
-        return [tab.tab_name for tab in self.tabs]
 
     def get_tab_pos_by_id(self, tab_id: str) -> int:
         for idx, tab in enumerate(self.tabs):
@@ -88,25 +80,13 @@ class CDetailsFrame(urwid.Frame):
             return self.tabs[tab_pos].tab_id
         return ""
 
-    def get_tab_pos_by_name(self, tab_name: str) -> int:
-        for idx, tab in enumerate(self.tabs):
-            if tab.tab_name == tab_name:
-                return idx
-        return -1
-
-    def get_tab_name_by_pos(self, tab_pos: int) -> str:
-        if 0 <= tab_pos < len(self.tabs):
-            return self.tabs[tab_pos].tab_name
-        return ""
-
     def get_tab(self) -> int:
         return self.current_tab
 
-    def set_tab(self, tab: int) -> None:
-        # TODO merge with on_tab_click?
-        self.current_tab = tab
-        tab_name = self.get_tab_name_by_pos(self.current_tab)
-        self.on_tab_click(None, tab_name)
+    def set_tab(self, tab_pos: int) -> None:
+        self.header.update_selected_tab_button(tab_pos)
+        self.body.original_widget = self.tabs[tab_pos]
+        self.current_tab = tab_pos
 
     def next_tab(self):
         tab = (self.current_tab + 1) % len(self.tabs)
@@ -139,6 +119,10 @@ class CDetailsFrame(urwid.Frame):
 
     def get_count(self) -> int:
         return self.get_tab_body().get_count()
+
+    def on_tab_click(self, button, tab_id):
+        tab_pos = self.get_tab_pos_by_id(tab_id)
+        self.set_tab(tab_pos)
 
     def keypress(self, size, key):
         key = super(CDetailsFrame, self).keypress(size, key)
