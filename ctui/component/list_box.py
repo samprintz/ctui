a0d2ccee93710fae0f7bbfba00a35fb05bf722a9
Samuel Printz <sam.printz@web.de>
2024-12-24 12:31:26 +0100

    ref: add ListBox.get_command_map()

--- ctui/component/list_box.py
+++ ctui/component/list_box.py
@@ -15,38 +15,51 @@ class CListBox(urwid.ListBox):
         command_id, command_key, command_repeat \
             = self.core.keybindings.keypress(key, self.name)
 
-        match command_id:
-            case 'move_down':
-                self.jump_down(size, command_repeat)
-            case 'move_up':
-                self.jump_up(size, command_repeat)
-            case 'jump_to_first':
-                self.core.keybindings.set_simulating(True)
-                # 2x as workaround, otherwise list focus not updated
-                super(CListBox, self).keypress(size, 'home')
-                super(CListBox, self).keypress(size, 'home')
-                self.core.keybindings.set_simulating(False)
-            case 'jump_to_last':
-                self.core.keybindings.set_simulating(True)
-                # 2x as workaround, otherwise list focus not updated
-                super(CListBox, self).keypress(size, 'end')
-                super(CListBox, self).keypress(size, 'end')
-                self.core.keybindings.set_simulating(False)
-            case _:
-                self.core.keybindings.set(command_key, command_repeat)
-                self.core.keybindings.set_bubbling(True)
-                return key
+        # explicitly call this method of this class, otherwise the method of the
+        # inheritor class (ContactList/DetailList) would be called
+        if command_id in CListBox.get_command_map(self):
+            return CListBox.execute_command(self, command_id, command_repeat, size)
+        else:
+            self.core.keybindings.set(command_key, command_repeat)
+            self.core.keybindings.set_bubbling(True)
+            return key
 
-    def jump_down(self, size, n):
-        n = 1 if n == 0 else n  # at least once
-        self.core.keybindings.set_simulating(True)
-        for i in range(0, n):
-            super(CListBox, self).keypress(size, 'down')
-        self.core.keybindings.set_simulating(False)
+    def execute_command(self, command_id, command_repeat, size):
+        command = CListBox.get_command_map(self)[command_id]
+        return command(command_repeat, size)
 
-    def jump_up(self, size, n):
-        n = 1 if n == 0 else n  # at least once
-        self.core.keybindings.set_simulating(True)
-        for i in range(0, n):
-            super(CListBox, self).keypress(size, 'up')
-        self.core.keybindings.set_simulating(False)
+    def get_command_map(self):
+        def move_down(command_repeat, size):
+            n = 1 if command_repeat == 0 else command_repeat  # at least once
+            self.core.keybindings.set_simulating(True)
+            for i in range(0, n):
+                super(CListBox, self).keypress(size, 'down')
+            self.core.keybindings.set_simulating(False)
+
+        def move_up(command_repeat, size):
+            n = 1 if command_repeat == 0 else command_repeat  # at least once
+            self.core.keybindings.set_simulating(True)
+            for i in range(0, n):
+                super(CListBox, self).keypress(size, 'up')
+            self.core.keybindings.set_simulating(False)
+
+        def jump_to_first(command_repeat, size):
+            self.core.keybindings.set_simulating(True)
+            # 2x as workaround, otherwise list focus not updated
+            super(CListBox, self).keypress(size, 'home')
+            super(CListBox, self).keypress(size, 'home')
+            self.core.keybindings.set_simulating(False)
+
+        def jump_to_last(command_repeat, size):
+            self.core.keybindings.set_simulating(True)
+            # 2x as workaround, otherwise list focus not updated
+            super(CListBox, self).keypress(size, 'end')
+            super(CListBox, self).keypress(size, 'end')
+            self.core.keybindings.set_simulating(False)
+
+        return {
+            'move_down': move_down,
+            'move_up': move_up,
+            'jump_to_first': jump_to_first,
+            'jump_to_last': jump_to_last,
+        }
